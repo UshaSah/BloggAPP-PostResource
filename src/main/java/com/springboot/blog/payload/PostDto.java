78f6bf3cae3f910c2d066fe133d716d6aaf3bd0f
UshaSah <usah@ucdavis.edu>
2024-11-12 22:04:53 -0800

    added validating Post REST API response

--- src/main/java/com/springboot/blog/payload/PostDto.java
+++ src/main/java/com/springboot/blog/payload/PostDto.java
@@ -1,15 +1,28 @@
 package com.springboot.blog.payload;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+import org.hibernate.annotations.Polymorphism;
 
 import java.util.Set;
 
 @Data // internally generates getter, setters for all the fields
 public class PostDto {
     private long id;
+
+    //title can't be null or empty and should have at least 2 characters
+    @NotEmpty
+    @Size(min = 2, message = "Post title should have at least 2 characters")
     private String title;
+
+    @NotEmpty
+    @Size(min = 10, message = "Post title should have at least 10 characters")
     private String description;
+
+    @NotEmpty
     private String content;
+
     private Set<CommentDto> comments;
 
 }
