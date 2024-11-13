e25b09b1addff78999257f3545804f43ff74ebd1
UshaSah <usah@ucdavis.edu>
2024-11-12 19:07:45 -0800

    added Custom Exception Handling

--- src/main/java/com/springboot/blog/payload/PostDto.java
+++ src/main/java/com/springboot/blog/payload/PostDto.java
@@ -2,11 +2,14 @@ package com.springboot.blog.payload;
 
 import lombok.Data;
 
+import java.util.Set;
+
 @Data // internally generates getter, setters for all the fields
 public class PostDto {
     private long id;
     private String title;
     private String description;
     private String content;
+    private Set<CommentDto> comments;
 
 }
