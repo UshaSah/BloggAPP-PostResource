d8af716d0749e527c8b6dab8d6c56c3f966568e3
UshaSah <usah@ucdavis.edu>
2024-11-12 22:16:35 -0800

    added validating Comment REST API response

--- src/main/java/com/springboot/blog/payload/CommentDto.java
+++ src/main/java/com/springboot/blog/payload/CommentDto.java
@@ -1,12 +1,24 @@
 package com.springboot.blog.payload;
 
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CommentDto {
     private long id;
+
+    @NotEmpty(message = "Name cannot be empty")
     private String name;
+
+    @NotEmpty(message = "Email should not be null or empty")
+    @Email
     private String email;
+
+    @NotEmpty
+    @Size(min = 10, message = "Comment body should be atl east 10 characters")
     private String body;
 }
